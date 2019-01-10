54e3be7aacb280532fac3171ae22e720513f27dd
TYDFC <10863265@qq.com>
2019-01-10 15:22:59 +0800

    Initial commit

---
+++ src/main/java/com/think/app/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.think.app.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello world!!!";
+    }
+
+}
